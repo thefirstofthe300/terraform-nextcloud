ad79dff4c541c8e97d530f16035de8859cf37edd
Danny Seymour <danny@seymour.family>
2021-06-03 02:45:33 -0700

    feat: destroy unnused Nextcloud instance again

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ resource "digitalocean_vpc" "nextcloud" {
   ip_range = "10.0.0.0/24"
 }
 
-resource "digitalocean_droplet" "nextcloud" {
-  image              = "debian-10-x64"
-  name               = "nextcloud-${digitalocean_vpc.nextcloud.region}-1"
-  region             = digitalocean_vpc.nextcloud.region
-  size               = "s-2vcpu-4gb"
-  vpc_uuid           = digitalocean_vpc.nextcloud.id
-  monitoring         = true
-  private_networking = true
-  tags               = ["nextcloud"]
-  ssh_keys           = data.digitalocean_ssh_keys.keys.ssh_keys[*].id
-}
+# resource "digitalocean_droplet" "nextcloud" {
+#   image              = "debian-10-x64"
+#   name               = "nextcloud-${digitalocean_vpc.nextcloud.region}-1"
+#   region             = digitalocean_vpc.nextcloud.region
+#   size               = "s-2vcpu-4gb"
+#   vpc_uuid           = digitalocean_vpc.nextcloud.id
+#   monitoring         = true
+#   private_networking = true
+#   tags               = ["nextcloud"]
+#   ssh_keys           = data.digitalocean_ssh_keys.keys.ssh_keys[*].id
+# }
 
 resource "digitalocean_spaces_bucket" "nextcloud" {
   name   = "cloud-seymour-family"
@@ -29,7 +29,7 @@ resource "digitalocean_project" "nextcloud" {
   purpose     = "Web Application"
   environment = "Production"
   resources = [
-    digitalocean_droplet.nextcloud.urn,
+    # digitalocean_droplet.nextcloud.urn,
     digitalocean_spaces_bucket.nextcloud.urn
   ]
 }
