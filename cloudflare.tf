6edba896dfda89c0dd6011bdc42dad43a27b5db1
Danny Seymour <danny@seymour.family>
2021-06-03 03:03:05 -0700

    feat: add CloudFlare DNS updates

---
+++ cloudflare.tf
@@ -0,0 +1,15 @@
+resource cloudflare_zone seymour_family {
+    zone = "seymour.family"
+}
+
+resource cloudflare_zone_dnssec seymour_family {
+    zone_id = cloudflare_zone.seymour_family.id
+}
+
+resource "cloudflare_record" "nextcloud" {
+  zone_id = cloudflare_zone.seymour_family.id
+  name    = "cloud"
+  value   = digitalocean_droplet.nextcloud.ipv4_address
+  type    = "A"
+  ttl     = 300
+}
