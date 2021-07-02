cc9dcc09c5f81147c9eae3b239aa5d493b5c1472
Danny Seymour <danny@seymour.family>
2021-07-02 01:02:15 -0700

    feat: add DNS records

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,9 +1,9 @@
-resource cloudflare_zone seymour_family {
-    zone = "seymour.family"
+resource "cloudflare_zone" "seymour_family" {
+  zone = "seymour.family"
 }
 
-resource cloudflare_zone_dnssec seymour_family {
-    zone_id = cloudflare_zone.seymour_family.id
+resource "cloudflare_zone_dnssec" "seymour_family" {
+  zone_id = cloudflare_zone.seymour_family.id
 }
 
 # resource "cloudflare_record" "nextcloud" {
@@ -13,3 +13,11 @@ resource cloudflare_zone_dnssec seymour_family {
 #   type    = "A"
 #   ttl     = 300
 # }
+
+resource "cloudflare_record" "paperless" {
+  zone_id = cloudflare_zone.seymour_family.id
+  name    = "paperless"
+  value   = digitalocean_droplet.paperless.ipv4_address
+  type    = "A"
+  ttl     = 300
+}
