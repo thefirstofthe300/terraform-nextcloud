8f0bc6695bd7b99442e48dae4c28471208470df3
Danny Seymour <danny@seymour.family>
2021-09-16 20:33:13 -0700

    feat: actually destroy nextcloud instance

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,14 +6,6 @@ resource "cloudflare_zone_dnssec" "seymour_family" {
   zone_id = cloudflare_zone.seymour_family.id
 }
 
-resource "cloudflare_record" "nextcloud" {
-  zone_id = cloudflare_zone.seymour_family.id
-  name    = "cloud"
-  value   = digitalocean_droplet.nextcloud.ipv4_address
-  type    = "A"
-  ttl     = 300
-}
-
 resource "cloudflare_record" "paperless" {
   zone_id = cloudflare_zone.seymour_family.id
   name    = "paperless"
